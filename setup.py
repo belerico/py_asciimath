6006c958735921dbcf8c447a501c096c1c06799f
belerico <belo.fede@outlook.com>
2020-04-06 20:29:53 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/belerico/py-asciimath",
-    packages=["py_asciimath", "tests"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
