6f43c6b396e3bc60513906560f2e8cdd46175c85
belerico <belo.fede@outlook.com>
2020-04-07 18:06:26 +0200

    Fix python2.7 compatibility

--- py_asciimath/transformer/const.py
+++ py_asciimath/transformer/const.py
@@ -1,7 +1,10 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
 
-from builtins import dict
 from itertools import islice
 
 # # from future import standard_library
