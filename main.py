f03d133425e5a2aeaa35fc763612d55bad60b344
belerico <belo.fede@outlook.com>
2020-04-07 11:04:37 +0200

    Fix main

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from py_asciimath.parser.parser import ASCIIMath2Tex
 from py_asciimath.parser.const import asciimath_grammar
 
 if __name__ == "__main__":
-    parser = ASCIIMath2Tex(
-        asciimath_grammar,
-        inplace=False,
-        parser="lalr",
-        lexer="contextual",
-    )
+    parser = ASCIIMath2Tex(asciimath_grammar, inplace=False,)
     text = ""
     text = (
         text
@@ -28,7 +23,8 @@ if __name__ == "__main__":
         + """
         uuu_{2(x+1)=1)^{n}
         min{
-                2x|x^{y+2} in bbb(N) wedge arccos root(3}(frac{1}{3x}) < i rarr Omega < b, 5=x
+                2x|x^{y+2} in bbb(N) wedge arccos root(3}(frac{1}{3x}) < i
+                rarr Omega < b, 5=x
         }
     """
     )
@@ -42,14 +38,24 @@ if __name__ == "__main__":
     text = text + """||:[2 x + 17 y = 23],[y = int_{0}^{x} t dt]:||"""
     text = (
         text
-        + """floor frac "Time" (A nn (bbb(N) | f'(x) = dx/dy | |><|><| (D setminus (B uu C))))"""
+        + """
+            floor frac "Time" (A nn (bbb(N) | f'(x) = dx/dy | |><|><|
+            (D setminus (B uu C))))
+        """
     )
     text = text + """(1,2,3)"""
+    # System of equation
     text = (
         text
-        + """e^{:[2 x + 17 (y) = 23], [1], [y = dstyle int_{0}^{x} t dt], [y = dstyle integral_{0}^{x} t dt]:}"""
+        + """
+            e^{:
+                [2 x + 17 (y) = 23],
+                [1],
+                [y = dstyle int_{0}^{x} t dt],
+                [y = dstyle integral_{0}^{x} t dt]
+            :}"""
     )
     text = text + """((1,2))int sin{x^2}/4pidxroot(5)(x_1^2+x_2^2)"""
     text = text + "[(1,2), (2^|: 3 :|, (dstyle int x^{2(x-n)})), (2,4)]"
-    text = text + "[[int x dx], [log(x+1)]]"
+    text = text + "[[], []]"
     print(parser.asciimath2tex(text, pprint=True))
