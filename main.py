daa153f0b5309c91a626c65c7cfd1b9a50081d33
belerico <belo.fede@outlook.com>
2020-04-07 11:59:42 +0200

    Fix main imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from py_asciimath.parser.parser import ASCIIMath2Tex
-from py_asciimath.parser.const import asciimath_grammar
+from py_asciimath.transformer.const import asciimath_grammar
 
 if __name__ == "__main__":
     parser = ASCIIMath2Tex(asciimath_grammar, inplace=False,)
