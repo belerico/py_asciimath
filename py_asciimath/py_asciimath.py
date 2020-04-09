3bb7fd1f54b1df6bdf8d11f0621f217db26eb5dd
belerico <belo.fede@outlook.com>
2020-04-10 01:00:03 +0200

    Add command line arguments to support mathml translation

--- py_asciimath/py_asciimath.py
+++ py_asciimath/py_asciimath.py
@@ -1,7 +1,8 @@
 """py_asciimath: a simple ASCIIMath converter.
 
 Usage:
-  py_asciimath.py ASCIIMATH ... (-o OLANG | --output=OLANG) [--log]
+  py_asciimath.py ASCIIMATH ... (-o OLANG | --output=OLANG)
+            [--log] [--pprint] [--to-file]
   py_asciimath.py (-h | --help)
   py_asciimath.py --version
 
@@ -9,12 +10,17 @@ Options:
   -h --help                     Show this screen.
   -o OLANG --output=OLANG       Output language.
   --log                         Log the transformation process.
+  --pprint                      Pretty print
+  --to-file                     Save output to file
   --version                     Show version.
 """
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
 # from future import standard_library
 # standard_library.install_aliases()
 import sys
@@ -22,24 +28,38 @@ import sys
 from docopt import docopt
 
 from . import __version__
-from .parser.parser import ASCIIMath2Tex
-from .transformer.const import asciimath_grammar
-from .transformer.transformer import LatexTransformer
+from .grammar.asciimath_grammar import asciimath_grammar
+from .parser.parser import ASCIIMath2MathML, ASCIIMath2Tex
+from .transformer.transformer import LatexTransformer, MathMLTransformer
 
 
 def main():
     arguments = docopt(__doc__, version="py_asciimath " + __version__)
     if arguments["--output"]:
-        if arguments["--output"].lower() == "latex":
+        olang = arguments["--output"].lower()
+        if olang == "latex":
             print("Translating ...")
             print(
                 ASCIIMath2Tex(
                     asciimath_grammar,
                     transformer=LatexTransformer(log=arguments["--log"]),
-                ).asciimath2tex(" ".join(arguments["ASCIIMATH"]))
+                ).translate(
+                    " ".join(arguments["ASCIIMATH"]), arguments["--pprint"]
+                )
+            )
+        elif olang == "mathml":
+            print("Translating ...")
+            print(
+                ASCIIMath2MathML(
+                    asciimath_grammar,
+                    transformer=MathMLTransformer(log=arguments["--log"]),
+                ).translate(
+                    " ".join(arguments["ASCIIMATH"]),
+                    pprint=arguments["--pprint"],
+                )
             )
         else:
-            print("SUPPORTED OLANG: 'latex'", file=sys.stderr)
+            print("SUPPORTED OLANG: 'latex', 'mathml'", file=sys.stderr)
             sys.exit(1)
     else:
         print(arguments)
