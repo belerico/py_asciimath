e9c032ef9734f17ffbfdcb8048a984fee463c69c
belerico <belo.fede@outlook.com>
2020-04-10 21:00:13 +0200

    Add offline validation against DTD

--- py_asciimath/parser/parser.py
+++ py_asciimath/parser/parser.py
@@ -12,8 +12,8 @@ import lxml.etree
 # from future import standard_library
 from lark import Lark
 
+from .. import PROJECT_ROOT
 from ..transformer.transformer import LatexTransformer, MathMLTransformer
-from ..utils.utils import check_connection, httplib
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
@@ -98,26 +98,62 @@ class ASCIIMath2MathML(ASCIIMathTranslator):
             grammar, *args, transformer=MathMLTransformer(log=log), **kwargs
         )
 
-    def __dtd_validation(self, xml, dtd_validation):
-        if check_connection():
-            logging.info("LOADING DTD...")
-            lxml_parser = lxml.etree.XMLParser(
-                dtd_validation=dtd_validation,
-                no_network=False,
-                load_dtd=True,
-                ns_clean=True,
-                remove_blank_text=True,
+    def __dtd_validation(self, xml, dtd_validation, conn):
+        print(xml)
+        logging.info("LOADING DTD...")
+        lxml_parser = lxml.etree.XMLParser(
+            dtd_validation=dtd_validation,
+            no_network=(not conn),
+            load_dtd=True,
+            ns_clean=True,
+            remove_blank_text=True,
+            resolve_entities=False,
+        )
+        logging.info(
+            "PARSING{}XML...".format(
+                " AND VALIDATING " if dtd_validation else " "
             )
-            logging.info(
-                "PARSING{}XML...".format(
-                    " AND VALIDATING " if dtd_validation else " "
+        )
+        return lxml.etree.fromstring(xml, lxml_parser)
+
+    def __get_dtd_head(self, dtd, conn):
+        dtd_head = "<!DOCTYPE math {}>"
+        if dtd is None or dtd.lower() == "mathml3":
+            dtd_head = dtd_head.format(
+                'PUBLIC "-//W3C//DTD MathML 3.0//EN" '
+                + '"http://www.w3.org/Math/DTD/mathml3/mathml3.dtd"'
+                if conn
+                else "SYSTEM "
+                + '"'
+                + PROJECT_ROOT
+                + '/translation/dtd/mathml3/mathml3.dtd"'
+            )
+        elif dtd.lower() == "mathml1":
+            dtd_head = dtd_head.format(
+                "SYSTEM "
+                + (
+                    '"http://www.w3.org/Math/DTD/mathml1/mathml.dtd"'
+                    if conn
+                    else '"'
+                    + PROJECT_ROOT
+                    + '/translation/dtd/mathml1/mathml1.dtd"'
                 )
             )
-            return lxml.etree.fromstring(xml, lxml_parser)
-        else:
-            raise httplib.HTTPException(
-                "No connection available to check against DTD"
+        elif dtd.lower() == "mathml2":
+            dtd_head = dtd_head.format(
+                'PUBLIC "-//W3C//DTD MathML 2.0//EN" '
+                + '"http://www.w3.org/Math/DTD/mathml2/mathml2.dtd"'
+                if conn
+                else "SYSTEM "
+                + '"'
+                + PROJECT_ROOT
+                + '/translation/dtd/mathml2/mathml2.dtd"'
             )
+        else:
+            raise NotImplementedError(
+                "DTD validation only against MathML DTD 1, 2 or 3"
+            )
+        return dtd_head
 
     def translate(
         self,
@@ -132,39 +168,19 @@ class ASCIIMath2MathML(ASCIIMathTranslator):
             dstyle = '<mstyle displaystyle="true">{}</mstyle>'
         else:
             dstyle = "{}"
-        if dtd is None or dtd.lower() == "mathml3":
-            dtd_head = (
-                '<!DOCTYPE math PUBLIC "-//W3C//DTD MathML 3.0//EN" '
-                + '"http://www.w3.org/Math/DTD/mathml3/mathml3.dtd">'
-            )
-        elif dtd.lower() == "mathml1":
-            dtd_head = (
-                "<!DOCTYPE math SYSTEM "
-                + '"http://www.w3.org/Math/DTD/mathml1/mathml.dtd">'
-            )
-        elif dtd.lower() == "mathml2":
-            dtd_head = (
-                '<!DOCTYPE math PUBLIC "-//W3C//DTD MathML 2.0//EN" '
-                + '"http://www.w3.org/Math/DTD/mathml2/mathml2.dtd">'
-            )
-        else:
-            raise NotImplementedError(
-                "DTD validation only against MathML DTD 1, 2 or 3"
-            )
+        dtd_head = self.__get_dtd_head(dtd, False)
         parsed = (
             dtd_head
-            + "<math"
             + (
-                " xmlns='http://www.w3.org/1998/Math/MathML'"
+                "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">"
                 if dtd != "mathml1"
-                else ""
+                else "<math>"
             )
-            + ">"
             + dstyle.format(super(ASCIIMath2MathML, self).translate(s, pprint))
             + "</math>"
         )
         if dtd_validation or xml_pprint:
-            parsed = self.__dtd_validation(parsed, dtd_validation)
+            parsed = self.__dtd_validation(parsed, dtd_validation, False)
             parsed = lxml.etree.tostring(
                 parsed, pretty_print=xml_pprint
             ).decode()
