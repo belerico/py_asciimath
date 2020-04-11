b225439188d460fa431d74974b1d7edcf3082334
belerico <belo.fede@outlook.com>
2020-04-11 15:39:39 +0200

    Add version 0.1.5

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/belerico/py-asciimath",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     entry_points={
         "console_scripts": ["py_asciimath=py_asciimath.py_asciimath:main"]
     },
