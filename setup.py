edfe06882972c86d5531d698bd61363c8d24c94a
belerico <belo.fede@outlook.com>
2020-04-16 14:10:47 +0200

    Fix pypi classifier

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setuptools.setup(
     test_suite="tests",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
